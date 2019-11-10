a46ee89cc626d4ffbffa767f0eeb899a2feccd51
PotatosFish <jimmyguding@gmail.com>
2019-11-10 09:08:23 -0600

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import { form, TextField } from '@material-ui/core';
+
+class Main extends React.Component {
+  state = {
+    pressed: false
+  }
+
+  render() {
+    if (this.state.pressed)
+      return (
+        <div className="App" style={{margin: 50}}>
+          <form>
+            <TextField id="a" label="asdf" autoComplete="abc cde def"/>
+          </form>
+        </div>
+      );
+    else
+      return (
+        <div className="App">
+          <button style={{width: "100%", border: 'none', padding: 0}} onClick={() => this.setState({pressed: true})}>
+            <header className="App-header">
+              <img src={"https://www.blueleaflending.com/wp-content/uploads/2018/11/fannie-mae-icon.png"} className="App-logo" alt="logo" />
+              <p style={{fontSize: 70}}>
+                Fannie Displae
+              </p>
+            </header>
+          </button>
+        </div>
+      );
+  }
+}
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <Main/>;
 }
 
 export default App;
